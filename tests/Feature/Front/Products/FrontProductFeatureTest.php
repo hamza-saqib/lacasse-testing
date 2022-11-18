a383ff3f3cf2e1df048f18de3e392340218c93b6
Hamza Saqib <mianhamza7262@gmail.com>
2022-11-18 18:35:44 +0500

    asdsad

--- tests/Feature/Front/Products/FrontProductFeatureTest.php
+++ tests/Feature/Front/Products/FrontProductFeatureTest.php
@@ -10,6 +10,8 @@ class FrontProductFeatureTest extends TestCase
     /** @test */
     public function it_can_show_the_product()
     {
+        dd('conc : ' .env('DB_CONNECTION') . 'db : ' . env('DB_DATABASE') . 'pass : ' . env('DB_PASSWORD') . ' user : ' . 
+    env('DB_USERNAME') . 'port : ' .env('DB_PORT')  . 'host : ' . env('DB_HOST'));
         $product = factory(Product::class)->create();
 
         $this
