b3db42c94458442c129859f5a315b668d8278cb4
Hamza Saqib <mianhamza7262@gmail.com>
2022-11-18 18:55:58 +0500

    asdsd

--- tests/Feature/Front/Products/FrontProductFeatureTest.php
+++ tests/Feature/Front/Products/FrontProductFeatureTest.php
@@ -10,8 +10,8 @@ class FrontProductFeatureTest extends TestCase
     /** @test */
     public function it_can_show_the_product()
     {
-        $asd = 'conc : ' .env('DB_CONNECTION') . 'db : ' . env('DB_DATABASE') . 'pass : ' . env('DB_PASSWORD') . ' user : ' . 
-        env('DB_USERNAME') . 'port : ' .env('DB_PORT')  . 'host : ' . env('DB_HOST');
+        $asd = 'conc :' .env('DB_CONNECTION') . ', db :' . env('DB_DATABASE') . ', pass :' . env('DB_PASSWORD') . ', user :' . 
+        env('DB_USERNAME') . ', port :' .env('DB_PORT')  . ', host :' . env('DB_HOST') . ',';
         dd($asd);
 
         dump($asd);
